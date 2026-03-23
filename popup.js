af88a94f6f3f1702a7f93f66671f9d973bb40d19
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-23 13:06:39 +0530

    nbahlo

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 document.getElementById("start").onclick = async () => {
   let [tab] = await chrome.tabs.query({
     active: true,
-    currentWindow: true
+    currentWindow: true,
   });
 
+  const time = document.getElementById("time").value;
+
   chrome.tabs.sendMessage(tab.id, {
-    action: "START"
+    action: "START",
+    time: Number(time) * 1000,
   });
 };
